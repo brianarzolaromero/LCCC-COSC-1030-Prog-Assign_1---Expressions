c2cae8e7552e9a4de4b00017509903889a07be93
brianarzolaromero <brianarzola@student.lccc.wy.edu>
2026-09-07 05:04:15 +0000

    Brian Arzola Romero

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl << endl;
+    cout << "1. Hello to the COSC 1030 course!" << endl;
+    cout << "2. My name is Brian Arzola Romero." << endl;
+    cout << "3. I am a second-year Computer Science student." << endl;
+    cout << "4. I enjoy learning about technology and AI." << endl;
+    cout << "5. I like listening to music and cooking." << endl;
+    cout << "6. I like playing video games and I enjoy fighting games." << endl;
+    cout << "7. I enjoy watching Marvel superhero movies." << endl;
+    cout << "8. I enjoy spending time with family and walking my dog." << endl;
+    cout << "9. I am excited to learn C++ in COSC 1030." << endl;
+    cout << "10. I want to improve my programming skills and try to create simple applications." << endl;
+
+    return 0;
+}
